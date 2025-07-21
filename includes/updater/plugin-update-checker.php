1cf483f9983d4ee8310f09d6ccb27ed6d63e0a76
atrixit2025 <atrixitsolution2025@gmail.com>
2025-07-21 20:19:35 +0530

    Update plugin-update-checker.php

--- includes/updater/plugin-update-checker.php
+++ includes/updater/plugin-update-checker.php
@@ -6,6 +6,7 @@ $myUpdateChecker = Puc_v5p1_Factory::buildUpdateChecker(
     __FILE__,
     'carousel-slider-plugin'
 );
+$myUpdateChecker->setBranch('main');
 // Tell PUC to use GitHub
 $myUpdateChecker->setAuthentication('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'); // Optional GitHub token if private repo
 
