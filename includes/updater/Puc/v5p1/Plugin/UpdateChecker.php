5e4c1ee6022d1e000fd6496d07ee1cf93ef88636
atrixit2025 <atrixitsolution2025@gmail.com>
2025-07-22 00:03:11 +0530

    update

--- includes/updater/Puc/v5p1/Plugin/UpdateChecker.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Puc_v5p1_Plugin_UpdateChecker {
-    public function __construct($metadataUrl, $pluginFile, $slug) {
-        // This is just a dummy class to simulate the PUC behavior.
-    }
-}
