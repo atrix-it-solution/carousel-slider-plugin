342fa9f657848b8c263f42eb778569a86143510a
atrixit2025 <atrixitsolution2025@gmail.com>
2025-07-21 22:57:15 +0530

    update

--- includes/update.php
+++ includes/update.php
@@ -1,5 +1,2 @@
 <?php
 require_once plugin_dir_path(__FILE__) . 'updater/plugin-update-checker.php';
-
-
-//hekiojasdna uhoadoic coZcxopzxcz  cpzxczx czpcozx cidf zxc nzc;zpc jxic fo  xczx czxoic zxcozxc 
